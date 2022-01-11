26e7b4c7ab7754484f8ae4715ceac4f67e1a6400
pvcapuano <pvcapuano@gmail.com>
2022-01-11 17:53:37 -0300

    repository item component

---
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -0,0 +1,10 @@
+export function RepositoryItem(props) {
+  return (
+    <li>
+      <strong>{props.repository}</strong>
+      <p>Forms in React</p>
+
+      <a href="">Acessar repositorios</a>
+    </li>
+  );
+}
--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,27 +1,15 @@
+import { RepositoryItem } from "./RepositoryItem";
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">Acessar repositorios</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">Acessar repositorios</a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">Acessar repositorios</a>
-        </li>
+        <RepositoryItem repository="unform22" />
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
       </ul>
     </section>
   );
