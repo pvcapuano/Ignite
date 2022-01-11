f3dd3b09d16c5e97b44f00983f66cb85f2a37f31
pvcapuano <pvcapuano@gmail.com>
2022-01-11 17:41:05 -0300

    sass config

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,28 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar repositorios</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar repositorios</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar repositorios</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
