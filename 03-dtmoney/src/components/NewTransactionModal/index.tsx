6e93bdf7d3cc0daa4679d57399d6ccc630a67c4a
pvcapuano <pvcapuano@gmail.com>
2022-02-24 18:06:08 -0300

    save form infos

--- 03-dtmoney/src/components/NewTransactionModal/index.tsx
+++ 03-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
@@ -12,7 +12,15 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionProps) {
   //states
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
+
+  const handleCreateNewTransaction = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(title, value, category, type);
+  };
 
   return (
     <Modal
@@ -21,11 +29,20 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar Transação</h2>
 
-        <input placeholder="Título" />
-        <input placeholder="Valor" type="number" />
+        <input
+          placeholder="Título"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <input
+          placeholder="Valor"
+          type="number"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -51,7 +68,11 @@ export function NewTransactionModal({
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input
+          placeholder="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
 
         <button type="submit">Cadastrar</button>
       </Container>
