18a69398a1e1b339870f8af3123975e87483867e
pvcapuano <pvcapuano@gmail.com>
2022-02-25 19:39:07 -0300

    format summary intl

--- 03-dtmoney/src/components/Summary/index.tsx
+++ 03-dtmoney/src/components/Summary/index.tsx
@@ -5,27 +5,61 @@ import { useContext } from "react";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
+
   return (
     <Container>
       <div>
         <header>
           <p>entradas</p>
         </header>
-        <strong>R$1000</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.deposits)}
+        </strong>
       </div>
 
       <div>
         <header>
           <p>saidas</p>
         </header>
-        <strong> - R$5000</strong>
+        <strong>
+          -{" "}
+          {new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.withdraws)}
+        </strong>
       </div>
 
       <div className="highlight-background">
         <header>
           <p>Total</p>
         </header>
-        <strong>R$1000</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
