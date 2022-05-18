dec26c5eaaae499edc04c275a85642a62c0c7d8b
Paulo Victor Capuano <pvcapuano@gmail.com>
2022-05-18 18:36:54 -0300

    Chapter III\11. Back-end no front-end\1. API Routes no Next.js finished

--- 04-ignews/src/components/SignInButton/index.tsx
+++ 04-ignews/src/components/SignInButton/index.tsx
@@ -1,18 +1,22 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signIn, signOut, useSession } from "next-auth/react"
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session, status } = useSession()
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  console.log(session)
+
+  return session ? (
+    <button type="button" className={styles.signInButton} onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      PV
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button type="button" className={styles.signInButton} 
+    onClick={() => signIn("github")}>
       <FaGithub color="#eba4c7" />
       Sign in with Github
     </button>
